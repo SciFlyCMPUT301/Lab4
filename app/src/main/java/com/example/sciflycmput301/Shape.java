8b95addde745859c9ef3c32167184c90563ed47d
Ximeng Duan <ximengduan@Macbook-3.local>
2024-09-25 18:42:10 -0600

    msg

--- app/src/main/java/com/example/sciflycmput301/Shape.java
+++ app/src/main/java/com/example/sciflycmput301/Shape.java
@@ -1,13 +1,9 @@
 package com.example.sciflycmput301;
 
 public class Shape {
-    String color = "black";
-
+    String color = "pink";
 
 public abstract class Shape {
     int x;
     int y;
-
-
-
 }
